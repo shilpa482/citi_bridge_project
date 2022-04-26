067b63429db20df8fedf3941ab8c4d2b55c96bd4
Vidisha Pandey <72399141+Vidisha780@users.noreply.github.com>
2022-04-26 18:27:04 +0530

    Update auth-service.service.ts

--- FrontEnd/src/app/auth-service.service.ts
+++ FrontEnd/src/app/auth-service.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { User } from './user';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,10 @@ export class AuthServiceService {
 
   constructor(private http:HttpClient) { }
 
-  logIn(data: any):Observable<any> {
+  logIn(user: User):Observable<any> {
+
     console.log("I am server");
-    console.log(JSON.stringify(data));
-    return this.http.post("http://localhost:8080/api/user/login", data);
+    console.log(JSON.stringify(user));
+    return this.http.post("http://localhost:8080/api/user/login", user);
   }
 }
